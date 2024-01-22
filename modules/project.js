227a7e63f8f8cdaff9297f38ae9c8f3dd81504f2
Vitaliy-Future-Developer <zarvoron@yandex.ru>
2024-01-22 22:28:12 +0300

    Изменяет роутинг и проект

--- modules/project.js
+++ modules/project.js
@@ -1,11 +1,12 @@
 const project = () => {
   const files = [];
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     files.push({
       id: 20000 * (i + 1),
-      avatarUrl: "string",
-      name: "string"
+      avatarUrl: "http://localhost:8081/src/assets/images/Demo.png",
+      name: `Проект ${i}`,
+      updateAt: ""
     })
   }
 
@@ -17,19 +18,22 @@ const project = () => {
           "VIEW_MEMBERS_PROJECT",  //экшен предназначен для отображения кнопки "Поделиться" на странице проекта, а также модального окна с участниками проекта,
           "VIEW_LINK_PROJECT",  // экшен предназначен для отображения кнопки "Скопировать ссылку" в меню проекта,
           "VIEW_SETTING_PROJECT",  // экшен предназначен для отображения кнопки "Настройки" в меню проекта, а также модального окна с названием и описание проекта,
-          "CREATE_FILE",  // экшен предназначен для отображения кнопки "Создать файл" на странице проекта
+          "CREATE_FILE_PROJECT",  // экшен предназначен для отображения кнопки "Создать файл" на странице проекта
           "DELETE_PROJECT_TEAM",  // экшен предназначен для отображения кнопки "Удалить проект" в меню проекта
           "INVITE_MEMBER_PROJECT",  // предназначен для отображения кнопки "Пригласить" в модальном окне "Поделиться"
           "REPEAT_INVITE_MEMBER_PROJECT" // предназначен для отображения кнопки "Пригласить повторно" в выпадающем меню участника, которому было отправлено приглашение, но участник его не принял.
       ],
-      projectId: 13,
-      projectName: "Spectr(Demo)",
-      projectDescription: "string",
-      isFavorite: false,
-      creatorId: 20,
-      createAt: "string",
-      updateAt: "string",
-      files: files
+      project: {
+        projectId: 13,
+        name: "Spectr(Demo)",
+        teamName: "Team myself",
+        projectDescription: "string",
+        isFavorite: false,
+        creatorId: 20,
+        createAt: "string",
+        updateAt: "string",
+        files: files
+      },
     }
   };
 }
@@ -43,12 +47,12 @@ const projectActionPatch = (req, res) => {
     const project = global.router.db.get('project').value();
 
     // Обновляем поле 'isFavorite' новым значением
-    project.parameters.isFavorite = !project.parameters.isFavorite;
+    project.parameters.project.isFavorite = !project.parameters.project.isFavorite;
 
     // Обновляем объект project в базе данных
     global.router.db.get('project').assign(project).write();
 
-    res.status(200).json(project.parameters.isFavorite);
+    res.status(200).json(project.parameters.project.isFavorite);
   } else {
     res.status(400).json({ error: 'Некорректный запрос' });
   }
